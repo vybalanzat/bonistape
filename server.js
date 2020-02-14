d6d1e3e12ff1ee6149aa8fdb3074d5b93c5e6eee
Vy Balanzat <vybalanzat@gmail.com>
2020-02-13 20:36:06 -0500

    test

--- server.js
+++ server.js
@@ -9,100 +9,81 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 var friends = [
-    {
-        routeName: "josh",
-        name: "josh",
-        scores: [1,2,3,4,5]
-
-    },
-    {
-
-        routeName: "jessah",
-        name: "jessah",
-        scores: [5,5,5,5,5]
-
-
-    },
-    {
-
-        routeName: "rick",
-        name: "rick",
-        scores: [3,3,3,3,3]
-
-
-    },
-    {
-
-        routeName: "james",
-        name: "james",
-        scores: [2,2,2,2,2]
-    }
-
+  {
+    routeName: "josh",
+    name: "josh",
+    scores: [1, 2, 3, 4, 5]
+  },
+  {
+    routeName: "jessah",
+    name: "jessah",
+    scores: [5, 5, 5, 5, 5]
+  },
+  {
+    routeName: "rick",
+    name: "rick",
+    scores: [3, 3, 3, 3, 3]
+  },
+  {
+    routeName: "james",
+    name: "james",
+    scores: [2, 2, 2, 2, 2]
+  }
 ];
 
 //routes to API
 
-app.get("/api/friends", function(req, res){
-    return res.json(friends);
+app.get("/api/friends", function(req, res) {
+  return res.json(friends);
+});
 
-})
-
-app.post("/api/friends", function(req, res){
-    var newFriend = req.body;
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-    console.log(friends);
-    friends.push(newFriend);
-    res.json(newFriend);
-
+app.post("/api/friends", function(req, res) {
+  var newFriend = req.body;
+  newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+  console.log(friends);
+  friends.push(newFriend);
+  res.json(newFriend);
 });
 
 //routes to HTML
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "public/home.html"));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/home.html"));
 });
 
-app.get("/survey", function(req, res){
-    res.sendFile(path.join(__dirname, "/public/survey.html"));
-
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/survey.html"));
 });
 
-app.post("/api/friends/compare", function(req, res){
-    var newFriend = req.body;
-    var matchedFriend = "";
-    var matchedlDiff = 0;
-    //index i is for friends array
-    for (var i = 0; i < friends.length; i++) {
-        //index y is for scores array
+app.post("/api/friends/compare", function(req, res) {
+  var newFriend = req.body;
+  var matchedFriend = "";
+  var matchedlDiff = 0;
+  //index i is for friends array
+  for (var i = 0; i < friends.length; i++) {
+    //index y is for scores array
 
-        var totalDiff = 100000;
+    var totalDiff = 100000;
 
-        for(var y = 0; y < friends[i].scores.length; y++) {
-           
-           var currentDiff =  Math.abs(newFriend.scores[y]) - parseInt(friends[i].scores[y]);
-            
-            totalDiff = Math.abs(currentDiff)+totalDiff;
-      
+    for (var y = 0; y < friends[i].scores.length; y++) {
+      var currentDiff =
+        Math.abs(newFriend.scores[y]) - parseInt(friends[i].scores[y]);
 
-       };
-        if(matchedlDiff === 0){
-            matchedlDiff = totalDiff;
-            matchedFriend = friends[i].name;
+      totalDiff = Math.abs(currentDiff) + totalDiff;
+    }
+    if (matchedlDiff === 0) {
+      matchedlDiff = totalDiff;
+      matchedFriend = friends[i].name;
+    }
+    if (totalDiff <= matchedlDiff) {
+      matchedFriend = friends[i].name;
+      matchedlDiff = totalDiff;
+      console.log(matchedFriend);
+    }
 
-        }
-       if (totalDiff <= matchedlDiff  ){
-            matchedFriend = friends[i].name;
-            matchedlDiff = totalDiff;
-            console.log(matchedFriend);
-       };
-
-       res.json(matchedFriend);
-
-    };
-
+    res.json(matchedFriend);
+  }
 });
 
-
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
-
